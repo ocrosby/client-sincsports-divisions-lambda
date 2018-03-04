82dcc48a593cbc781a2b69b8569ae5c18c85e2cd
omar.crosby <omar.crosby@gmail.com>
2018-03-03 20:32:37 -0500

    Refactoring...

--- src/index.js
+++ src/index.js
@@ -1,177 +1,24 @@
-const http = require('http');
-const bluebird = require('bluebird');
-const cheerio = require('cheerio');
-const querystring = require('querystring');
-
-global.Promise = bluebird.Promise;
-
-const CheerioOptions = {
-    withDomLvl1: true,
-    normalizeWhitespace: false,
-    xmlMode: false,
-    decodeEntities: true
-};
-
-const BoysSelector = 'tr:nth-child(19) a , #ctl00_ContentPlaceHolder1_MTable tr+ tr td';
-const GirlsSelector = '#ctl00_ContentPlaceHolder1_FTable tr+ tr td';
-
-
-exports.getTid = (season) => {
-    if (!season) {
-        return 'NCFL';
-    }
-
-    return season === 'fall' ? 'NCFL' : 'NCCSL';
-};
-
-exports.getSub = (season) => {
-    if (!season) {
-        return '3';
-    }
-
-    return season === 'fall' ? '3' : '2';
-};
-
-exports.generatePath = (season, year) => {
-    const tid = exports.getTid(season);
-    const sub = exports.getSub(season);
-
-    let path;
-
-    path = '/TTSchedules.aspx?';
-    path += `tid=${tid}&`;
-    path += 'tab=3&';
-    path += `sub=${sub}&`;
-    path += `sTid=${tid}&`;
-    path += `sYear=${year}`;
-
-    return path;
-};
-
-exports.getHTML = (season, year) => {
-    return new Promise((resolve, reject) => {
-        let request;
-        let options;
-        let path;
-
-        try {
-            path = exports.generatePath(season, year);
-
-            options = {
-                method: 'GET',
-                hostname: 'soccer.sincsports.com',
-                port: null,
-                path: path,
-                headers: {
-                    'cache-control': 'no-cache',
-                    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X)'
-                }
-            };
-
-            request = http.request(options, (response) => {
-                let chunks = [];
-
-                response.on("data", function (chunk) {
-                    chunks.push(chunk);
-                });
-
-                response.on("end", function () {
-                    let message;
-                    let body = chunks.join();
-
-                    if (response.statusCode === 200) {
-                        resolve(body.toString());
-                    } else {
-                        message = `Invalid status code ${response.statusCode}!`;
-                        reject(new Error(message));
-                    }
-                });
-            });
-        } catch(err) {
-            reject(err);
-        }
-    });
-};
-
-exports.createDivision = (item, gender) => {
-    let id;
-    let url;
-    let text;
-    let anchor;
-
-    text = item.text();
-    anchor = item.children('a');
-
-    if (anchor) {
-        url = anchor.attr('href');
-
-        if (url) {
-            id = querystring.parse(url).div;
-        } else {
-            id = url = '?';
-        }
-    } else {
-        id = url = '?';
-    }
-
-    return {
-        name: text,
-        url: url,
-        id: id,
-        gender: gender
-    };
-};
+const SincSportsService = require('./SincSportsService.js');
 
 exports.handler = (event, context, callback) => {
     const season = event.season || 'fall';
     const year = event.year || '2017';
 
-    exports.getHTML(season, year)
-        .then((html) => {
-            let i;
-            let $;
-            let promises;
-            let boysPromises;
-            let girlsPromises;
+    let service;
 
-            console.log('Retrieved the html.');
+    try {
+        service = new SincSportsService();
 
-            try {
-                $ = cheerio.load(html, CheerioOptions);
-            } catch (error) {
-                callback(error);
-            }
+        service.getDivisions(season, year)
+            .then((divisions) => {
+                callback(null, divisions);
+            })
+            .catch((err) => {
+                callback(err);
+            });
 
-            promises = [];
-
-            // Add the Boys Divisions
-            boysPromises = $(BoysSelector).map(function () {
-                return exports.createDivision($(this), 'male');
-            }).toArray();
-
-            girlsPromises = $(GirlsSelector).map(function () {
-                return exports.createDivision($(this), 'female');
-            }).toArray();
-
-            for (i = 0; i < boysPromises.length; i++) {
-                promises.push(boysPromises[i]);
-            }
-
-            for (i = 0; i < girlsPromises.length; i++) {
-                promises.push(girlsPromises[i]);
-            }
-
-            Promise.all(promises)
-                .then(function (data) {
-                    callback(null, data);
-                })
-                .fail(function (error) {
-                    callback(error);
-                });
-
-        })
-        .catch((err) => {
-            callback(err);
-        });
+    } catch(err) {
+        callback(err);
+    }
 };
 
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,54 +1,8 @@
 const index = require('../src/index');
-const sinon = require('sinon');
 const chai = require('chai');
 
 global.expect = chai.expect;
 
 describe('index', () => {
-    describe('getTid', () => {
-        it('returns "NCFL" when given "fall"', () => {
-            expect(index.getTid('fall')).to.equal('NCFL');
-        });
 
-        it('returns "NCCSL" when given "spring"', () => {
-            expect(index.getTid('spring')).to.equal('NCCSL');
-        });
-
-        it('returns "NCFL" when given nothing', () => {
-            expect(index.getTid()).to.equal('NCFL');
-        });
-
-        it('returns "NCFL" when given null', () => {
-            expect(index.getTid(null)).to.equal('NCFL');
-        });
-
-        it('returns "NCFL" when given undefined', () => {
-            expect(index.getTid(undefined)).to.equal('NCFL');
-        });
-    });
-
-    describe('getSub', () => {
-        it('returns "3" when given "fall"', () => {
-            expect(index.getSub('fall')).to.equal('3');
-        });
-
-        it('returns "3" when given nothing', () => {
-            expect(index.getSub()).to.equal('3');
-        });
-
-        it('returns "2" when given "spring"', () => {
-            expect(index.getSub('spring')).to.equal('2');
-        });
-    });
-
-    describe('generatePath', () => {
-        it('returns the expected path when given "fall" 2017', () => {
-            const expected = '/TTSchedules.aspx?tid=NCFL&tab=3&sub=3&sTid=NCFL&sYear=2017';
-            expect(index.generatePath('fall', 2017)).to.equal(expected);
-        });
-    });
-
-    describe('createDivision', () => {
-
-    });
 });
