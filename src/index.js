a0d4b5e9d9aa8566e7adb94151a1fd425c365873
omar.crosby <omar.crosby@gmail.com>
2018-03-09 04:43:36 -0500

    Attempting to update the handler to deal with parameters passed from the API gateway.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const SincSportsService = require('sincsports-service');
 
 exports.handler = (event, context, callback) => {
-    const season = event.season || 'fall';
-    const year = event.year || '2017';
+    const season = event.season || event.body.season || 'fall';
+    const year = event.year || event.body.year || '2017';
 
-    let service;
+    console.log(`Received event:\n${event}`);
 
     try {
         SincSportsService.getDivisions(season, year)
