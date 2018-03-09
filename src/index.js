9e116bce11731c0c1a4526cb8a5fa5cc68fed5fa
omar.crosby <omar.crosby@gmail.com>
2018-03-09 08:00:58 -0500

    Updating the handler so it extracts the path parameters as expected.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 const SincSportsService = require('sincsports-service');
 
 exports.handler = (event, context, callback) => {
-    const season = event.season || event.body.season || 'fall';
-    const year = event.year || event.body.year || '2017';
+    let season;
+    let year;
 
-    console.log(`Received event:\n${event}`);
+    console.log(`Received event:\n${JSON.stringify(event)}`);
+
+    season = event.pathParameters.season;
+    year = event.pathParameters.year;
+
+    console.log(`Retrieving SincSports divisions for the ${season} of ${year} ...`);
 
     try {
         SincSportsService.getDivisions(season, year)
