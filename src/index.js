3b12d382023648374a24b44ea75906a345104d20
omar.crosby <omar.crosby@gmail.com>
2018-03-10 08:37:03 -0500

    Refactoring the handler to move more statements into the try catch block.

--- src/index.js
+++ src/index.js
@@ -2,18 +2,19 @@ const SincSportsService = require('sincsports-service');
 const Helpers = require('lambda-helpers');
 
 exports.handler = (event, context, callback) => {
+    const responder = Helpers.GatewayResponder.Create(callback);
+
     let season;
     let year;
-    let responder = Helpers.GatewayResponder.Create(callback);
-
-    console.log(`Received event:\n${JSON.stringify(event)}`);
-
-    season = event.pathParameters.season;
-    year = event.pathParameters.year;
-
-    console.log(`Retrieving SincSports divisions for the ${season} of ${year} ...`);
 
     try {
+        console.log(`Received event:\n${JSON.stringify(event)}`);
+
+        season = event.pathParameters.season;
+        year = event.pathParameters.year;
+
+        console.log(`Retrieving SincSports divisions for the ${season} of ${year} ...`);
+
         SincSportsService.getDivisions(season, year)
             .then((divisions) => {
                 responder.success(divisions);
